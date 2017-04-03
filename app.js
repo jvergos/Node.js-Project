05ea373ce89f4a20e6a90d6e032f8f6d3d85bc61
nps <sancroth@gmail.com>
2017-04-04 00:52:55 +0300

    added all requested endpoints(no promises yet)

--- app.js
+++ app.js
@@ -1,12 +1,65 @@
 let express = require('express');
 let app = express();
 
+
+function eventEx(id, title, description, date) {
+    this.id = id;
+    this.title = title;
+    this.description = description;
+    this.date = date;
+}
+
+let events = [
+new eventEx(0,"randomEvent0","testing event 0","03042017"),
+new eventEx(1,"randomEvent1","testing event 1","03042017"),
+new eventEx(2,"randomEvent2","testing event 2","03042017"),
+new eventEx(3,"randomEvent3","testing event 3","03042017")
+];
+console.log(events);
+
 app.get('/', (req, res) => {
   res.send('Hello World! - Get');
 });
 
+app.get('/events/?', (req, res) => {
+  res.send(events);
+});
+
+app.get('/events/:id', (req, res) => {
+  let id = req.params.id;
+  res.send(events[id]);
+});
+
 app.post('/', (req, res) => {
   res.send('Hello World! - Post');
+});
+
+app.post('/events/:id/title/:title/desc/:desc/date/:date' , (req , res) =>{
+	let id = req.params.id;
+	let desc = req.params.desc;
+	let title = req.params.title;
+	let date = req.params.date;
+	let newEvent = new eventEx(id,title,desc,date);
+	events.push(newEvent);
+	res.send("New event added.");
+});
+
+app.put('/events/:id/title/:title/desc/:desc/date/:date' , (req , res) =>{
+	let id = req.params.id;
+	events[id].desc = req.params.desc;
+	events[id].title = req.params.title;
+	events[id].date = req.params.date;
+	res.send(events[id]);
+});
+
+app.delete('/events/:id', (req, res) => {
+  let id = req.params.id;
+  for (let i in events) {
+  		if(events[i].id == id){
+			  events.splice(i,1);  			
+  		}
+  }
+  res.send(events);
 });
 
 app.listen(3000, () => {
